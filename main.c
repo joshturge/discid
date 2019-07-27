218d30789fc5962af8804417fcbb1dce588b6861
joshturge <joshturge@hotmail.com>
2019-07-27 22:56:29 +1000

    Added comments

--- main.c
+++ main.c
@@ -5,10 +5,12 @@ int main(int argc, char *argv[]) {
     DiscId *disc = discid_new();
     char *device = NULL;
 
+    // If args use [1] as device 
     if (argc > 1) {
 		device = argv[1];
 	}
 
+    // Read the disc's TOC and check for errors
     if (discid_read_sparse(disc, device, 0) == 0) {
         fprintf(stderr, "Error: %s\n", discid_get_error_msg(disc));
         discid_free(disc);
